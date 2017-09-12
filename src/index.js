870481a719e3724d660a97686d8f01f9e006763d
mattperkins <github@mattperkins.com>
2017-09-12 01:42:17 +0100

    update styling

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 const LemonStyle = {
- color: 'pink',
+ color: 'red',
  fontFamily: 'sans-serif',
  background: '#f7f7f7',
  fontSize: 24,
  display: 'inline-block',
+ padding: 5,
 }
 const Lemon = ({lemon}) => 
  <p style={LemonStyle}>{lemon}</p>
